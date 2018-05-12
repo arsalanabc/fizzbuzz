120aedffaee6778465c922da5445e6da7a393bd8
Arsalan <arsalanabc@gmail.com>
2018-05-12 18:28:44 +0100

    added rest of the tests and coded the fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,15 @@
 def fizzbuzz(number)
-  "fizz"
+  if number == 3
+    "fizz"
+  elsif number == 5
+    "buzz"
+  elsif number % 5 == 0 &&  number % 3 == 0
+    "fizzbuzz"
+  elsif number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
+    "buzz"
+  else
+    number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,40 @@
-require 'fizzbuzz'
+require "fizzbuzz"
 
 describe 'fizzbuzz' do
   it "return 'fizz' when given 3" do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it "return 'buzz' when given 5" do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it "return 'buzz' when given multple of 5" do
+    expect(fizzbuzz(20)).to eq 'buzz'
+  end
+
+  it "return 'buzz' when given multple of 5" do
+    expect(fizzbuzz(25)).to eq 'buzz'
+  end
+
+  it "return 'fizzbuzz' when given multiple of 3 and 5" do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it "return 'fizz' when given multiple of 3" do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+
+  it "return 'fizz' when given multiple of 3" do
+    expect(fizzbuzz(18)).to eq 'fizz'
+  end
+
+  it "return non multiple of 3 and 5" do
+    expect(fizzbuzz(17)).to eq 17
+  end
+
+  it "return non multiple of 3 and 5" do
+    expect(fizzbuzz(26)).to eq 26
+  end
+
 end
